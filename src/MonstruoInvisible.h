1a0aa4a7bbc9ad3ae93e2399c3048d1e36142b90
DANIEL ESCOBAR GIRALDO <daniel.escobargiraldo@ucr.ac.cr>
2022-05-13 11:23:11 -0600

    clase vacia

---
+++ src/MonstruoInvisible.h
@@ -0,0 +1,11 @@
+#ifndef MONSTRUOPINVISIBLE_H
+#define MONSTRUOPINVISIBLE_H
+
+class MonstruoInvisible {
+ private:
+    /* data */
+ public:
+    MonstruoInvisible(/* args */);
+    ~MonstruoInvisible();
+};
+#endif // MONSTRUOPINVISIBLE_H
