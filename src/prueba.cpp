f4d36118a6f6f7e6cde71810e6cdb2d0ab03bc80
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-06-03 15:44:00 -0600

    amplio prueba

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -12,11 +12,30 @@ bool isOperator(string symbol) {
   return (symbol == "+" || symbol == "-" || symbol == "/" || symbol == "*");
 }
 
+void printStack(stack<Operando*> pila) {
+  cout << "Pila: ";
+  // como es por copia, no le afecta el pop
+  while( !pila.empty() )
+  {
+    Operando& operando = *pila.top();
+    pila.pop();
+    cout << operando.toString() << "  ";
+  }
+  cout << "\n"<< endl;
+}
+
+void printMap(map<string, Operando&> map) {
+  auto iter = map.begin();
+    while (iter != map.end()) {
+        cout << "[" << iter->first << ","
+                    << iter->second.toString() << "]\n";
+        ++iter;
+    }
+    cout << "\n" << endl;
+}
+
 Operando& operar(Operando& operando1, Operando& operando2, char oper) {
   switch (oper) {
-  case '+':
-    return operando1 + operando2;
-    break;
   case '-':
     return operando1 - operando2;
     break;
@@ -27,22 +46,25 @@ Operando& operar(Operando& operando1, Operando& operando2, char oper) {
     return operando1 / operando2;
     break;
   default:
-    static Doble op(0.0);
-    return op;
+    return operando1 + operando2;
     break;
   }
 }
 int main() {
   map<string,Operando&> variables;
 
-  Doble d1(2.2);
-  Doble d2(4.4);
-  Doble general();
+  // Doble d1(2.2);
+  // Doble d2(4.4);
+
+  Racional d1(5,6);
+  Racional d2(2,8);
 
   variables.insert(pair<string,Operando&>("d1",d1));
   variables.insert(pair<string,Operando&>("d2",d2));
+  // variables.insert(pair<string,Operando&>("d3",d2));
 
-  istringstream operacion("d1 d2 +");
+  istringstream operacion("d1 d2 / d1 + d1 - d1 +");
+  // 2.2 / 4.4 - 2.2
   cout << "oper: " << operacion.str() << endl;
   
   stack<Operando*> pila;
@@ -50,23 +72,27 @@ int main() {
   while(!operacion.eof()) {
     operacion >> symbol;
     if (isOperator(symbol)) {
+      cout << "Calculando " << symbol << " ";
+
       Operando& op1 = *pila.top();
       pila.pop();
       Operando& op2 = *pila.top();
       pila.pop();
 
-      Operando& result = operar(op1, op2, symbol[0]);
+      Operando& result = operar(op2, op1, symbol[0]);
       pila.push(&result);
-
-      cout << "operacion resuelta: " << op1.toString() << " " << symbol << " " << op2.toString() << " = " << result.toString() << endl;
     } 
-
     else {
       pila.push(&variables.at(symbol));
-      cout << "agregado a la pila: " << variables.at(symbol).toString() << endl;
     }
     // cout << "n: " << n << endl;
-
+    printStack(pila);
   }
+  
+  cout << "pila al final: " << endl;
+  printStack(pila);
+
+  cout << "map al final: " << endl;
+  printMap(variables);
   return 0;
 }
