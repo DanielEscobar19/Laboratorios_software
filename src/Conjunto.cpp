6449d2190ffea44d45c49fd9799ce0bd44d264b7
manuCR <manuelarroyoportilla@gmail.com>
2022-06-04 09:00:43 -0600

    cambio en conjunto

--- src/Conjunto.cpp
+++ src/Conjunto.cpp
@@ -19,46 +19,46 @@ string Conjunto::toString() {
 }
 
 Conjunto& Conjunto::operator+(Operando& otro ) {
-  static Conjunto resultado;
+  Conjunto * resultado = new Conjunto();
 
   Conjunto * conjunto = dynamic_cast<Conjunto*>(&otro);
   vector<int> v2 ( conjunto->valor.size() + this->valor.size() );
   std::vector<int>::iterator it = set_union(this->valor.begin(), this->valor.end(), conjunto->valor.begin(), conjunto->valor.end(), v2.begin());
   v2.resize(it-v2.begin());
-  std::copy(v2.begin(), v2.end(), std::inserter(resultado.valor,resultado.valor.end()));
-  return resultado;
+  std::copy(v2.begin(), v2.end(), std::inserter(resultado->valor,resultado->valor.end()));
+  return *resultado;
 }
 
 Conjunto& Conjunto::operator-(Operando& otro ) {
-  static Conjunto resultado;
+  Conjunto * resultado = new Conjunto();
 
   Conjunto * conjunto = dynamic_cast<Conjunto*>(&otro);
   vector<int> v2 ( conjunto->valor.size() + this->valor.size() );
   std::vector<int>::iterator it = set_difference(this->valor.begin(), this->valor.end(), conjunto->valor.begin(), conjunto->valor.end(), v2.begin());
   v2.resize(it-v2.begin());
-  std::copy(v2.begin(), v2.end(), std::inserter(resultado.valor,resultado.valor.end()));
-  return resultado;
+  std::copy(v2.begin(), v2.end(), std::inserter(resultado->valor,resultado->valor.end()));
+  return *resultado;
 }
 
 Conjunto& Conjunto::operator*(Operando& otro) {
-  static Conjunto resultado;
+  Conjunto * resultado = new Conjunto();
 
   Conjunto * conjunto = dynamic_cast<Conjunto*>(&otro);
   vector<int> v2 ( conjunto->valor.size() + this->valor.size() );
   std::vector<int>::iterator it = set_intersection(this->valor.begin(), this->valor.end(), conjunto->valor.begin(), conjunto->valor.end(), v2.begin());
   v2.resize(it-v2.begin());
-  std::copy(v2.begin(), v2.end(), std::inserter(resultado.valor,resultado.valor.end()));
-  return resultado;
+  std::copy(v2.begin(), v2.end(), std::inserter(resultado->valor,resultado->valor.end()));
+  return *resultado;
 }
 
 Conjunto& Conjunto::operator/(Operando& otro) {
-  static Conjunto resultado;
+  Conjunto * resultado = new Conjunto();
 
   Conjunto * conjunto = dynamic_cast<Conjunto*>(&otro);
   vector<int> v2 ( conjunto->valor.size() + this->valor.size() );
   std::vector<int>::iterator it = set_symmetric_difference(this->valor.begin(), this->valor.end(), conjunto->valor.begin(), conjunto->valor.end(), v2.begin());
   v2.resize(it-v2.begin());
-  std::copy(v2.begin(), v2.end(), std::inserter(resultado.valor,resultado.valor.end()));
-  return resultado;
+  std::copy(v2.begin(), v2.end(), std::inserter(resultado->valor,resultado->valor.end()));
+  return *resultado;
 }
 
